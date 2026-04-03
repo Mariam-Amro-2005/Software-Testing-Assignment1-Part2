738e2752a642446620ba6f26e8321a42385031fd
Mariam Amro <123351776+Mariam-Amro-2005@users.noreply.github.com>
2026-04-03 19:55:01 +0200

    Refactor test folder to Tests

--- Scrubbing/src/Tests/DigitScrubberTest.java
+++ Scrubbing/src/Tests/DigitScrubberTest.java
@@ -1,4 +1,4 @@
-package test;
+package Tests;
 
 import Services.DigitScrubber;
 import org.junit.jupiter.api.BeforeEach;
--- Scrubbing/src/Tests/EmailScrubberTest.java
+++ Scrubbing/src/Tests/EmailScrubberTest.java
@@ -1,4 +1,4 @@
-package test;
+package Tests;
 
 import Services.EmailScrubber;
 import org.junit.jupiter.api.BeforeEach;
